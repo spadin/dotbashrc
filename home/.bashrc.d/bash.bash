5526b4ac58db87132b21beea233512e61099dd6f
Sandro Padin <sandropadin@gmail.com>
2016-04-23 12:30:01 -0500

    Update bash

--- home/.bashrc.d/bash.bash
+++ home/.bashrc.d/bash.bash
@@ -1,3 +1,3 @@
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
+if [ -f /usr/local/etc/bash_completion ]; then
+  source /usr/local/etc/bash_completion
 fi
